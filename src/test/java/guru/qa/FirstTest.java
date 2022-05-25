71d68afa51f29bfccc4ee6d2705dd4daf99fcc06
kristina yeremeyeva <kristina_zhuravl@mail.ru>
2022-05-25 19:31:31 +0300

    added first test

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void FirstTest() {
+        Assertions.assertTrue(3 >= 0);
+    }
+}
